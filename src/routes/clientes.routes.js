f6dca2002ff4f1dd1b60221aff3e4d7942937925
andryross <andryelarossignol0@gmail.com>
2024-09-18 19:51:20 -0300

    Autenticação de Usuários

--- src/routes/clientes.routes.js
+++ src/routes/clientes.routes.js
@@ -1,11 +1,14 @@
 const { Router } = require('express');
 const ClientesController = require('../controllers/ClientesController');
+const ensureAuthenticated = require("../middlewares/ensureAuthenticated")
 
 const clientesRoutes = Router();
 const clientesController = new ClientesController();
 
+clientesRoutes.use(ensureAuthenticated);
+
 clientesRoutes.get('/', clientesController.index)
-clientesRoutes.post('/:user_id', clientesController.create)
+clientesRoutes.post('/', clientesController.create)
 clientesRoutes.get('/:id', clientesController.show)
 clientesRoutes.delete('/:id', clientesController.delete)
 
