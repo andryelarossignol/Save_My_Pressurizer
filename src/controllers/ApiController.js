226f2618085df86a829911de69832e612e7e081a
andryross <andryelarossignol0@gmail.com>
2024-09-30 14:16:44 -0300

    fix: fixing cliente response to Api

--- src/controllers/ApiController.js
+++ src/controllers/ApiController.js
@@ -1,16 +1,19 @@
 const mqtt = require("mqtt");
 
-const client = mqtt.connect("mqtt://119.8.146.205");
+const client = mqtt.connect("mqtt://119.8.14.205");
 let mqttMessage = "";
 
-client.on("message", (topic, message) => {
-    if (topic === "sensor/state") {
-        console.log("message: " +message)
-        mqttMessage = message;
-    }
-});
+
 
 class ApiController {
+    ApiController() {
+        client.on("message", (topic, message) => {
+            if (topic === "sensor/state") {
+                console.log("message: " + message)
+                mqttMessage = message;
+            }
+        });
+    }
     async getMessage(request, response) {
         return response.json(mqttMessage);
     }
